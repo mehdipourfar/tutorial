4b3a900a83fa5c8aac3c2ae1caefc48fc8962e31
Mehdi Pourfar <mehdipourfar@gmail.com>
2019-08-10 18:39:28 +0430

    crime statistics

---
+++ crime/main.py
@@ -0,0 +1,44 @@
+import csv
+
+def get_items_from_csv(file_path):
+    items = []
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            items.append(row)
+    return items
+
+
+def get_avg(items):
+    return int(sum(items) / len(items))
+
+
+class CrimeStatistics:
+    def __init__(self, crimes):
+        self.crimes = self.sanitize_data(crimes)
+
+    @staticmethod  # because we don't care about self
+    def sanitize_data(items):
+        return [
+            {
+                'year': int(item['Year']),
+                'population': int(item['Population']),
+                'murder': int(item['Murder']),
+                'robbery': int(item['Robbery']),
+                'assault': int(item['Assault']),
+                'burglary': int(item['Burglary']),
+                'car_theft': int(item['CarTheft']),
+            } for item in items
+        ]
+
+
+    def get_average_population(self):
+        return get_avg(
+            [item['population'] for item in self.crimes]
+        )
+
+
+if __name__ == '__main__':
+    crimes = get_items_from_csv('./crime.csv')
+    stats = CrimeStatistics(crimes)
+    print(stats.get_average_population())
