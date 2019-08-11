c188e44f1d8ca64b2e2d582453ecece907413595
Mehdi Pourfar <mehdipourfar@gmail.com>
2019-08-11 17:46:42 +0430

    add admin for post models

--- weblog/post/admin.py
+++ weblog/post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Category, Post, Tag, Comment, ButtonAction
+)
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(ButtonAction)
