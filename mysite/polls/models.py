29aae97ab46b57212d3da72bbae98f0e31854c8e
Mehdi Pourfar <mehdipourfar@gmail.com>
2019-08-11 11:12:41 +0430

    initial commit for polls app

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(
+        max_length=200
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Publish Date'
+    )
+
+    def __str__(self):
+        return f'({self.id}) {self.question_text}'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+    choice_text = models.CharField(
+        max_length=200,
+    )
+    votes = models.IntegerField(default=0)
